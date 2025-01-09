975bbb9bd1b610a82b026bfef842327c45d078a9
wtsachin <sachin.kolhe.evisiontech@gmail.com>
2025-01-09 16:54:54 +0530

    javascript data types

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// Variable declarations
+
+// let
+// - used to create varaible
+// - create mutable variables
+let num = 100
+console.log('num = ',num)
+
+num = 200
+console.log('num = ',num)
+
+// const
+// - used to create constant
+// - create immutable varibles
+const pi = 3.14
+console.log('pi = ',pi)
+// pi = 3.15 // can not change a value
---
+++ index1.js
@@ -0,0 +1,52 @@
+
+// data types
+// all data types in js are infered
+// infered : [will be dynamically assigned to the variable by inspecting value t the time]
+
+// number
+const num = 100
+console.log('num = ',num)
+console.log('data type of num =', typeof num)
+
+// number
+const salary = 5.6
+console.log('salary = ', salary)
+console.log('data type of salary = ', typeof salary)
+
+// string
+const firstName = 'Steve'
+console.log('firstName = ', firstName)
+console.log('data type of firstName = ', typeof firstName)
+
+//string
+const lastName = "Jobs"
+console.log('lastName = ',lastName)
+console.log('data type of lastName = ',typeof lastName)
+
+// string - `` [for create multiline string]
+const address = `home no 100,
+city parner,
+dist Ahmednagar,
+state Maharashra`
+console.log('address = ',address)
+console.log('data type of address = ', typeof address)
+
+// boolean
+const canVote = true
+console.log('canVote = ',canVote)
+console.log('data type of canVote = ', typeof canVote)
+
+//Object
+const person = {
+    name : 'person',
+    address : 'pune',
+    email : 'sachin.kolhe.evisiontech@gmail.com',
+    phone : '+91 9730651483',
+}
+console.log('person = ', person)
+console.log('type of person = ', typeof person)
+
+//undefined
+let myVar
+console.log('myVar = ', myVar)
+console.log('data type of myvar = ', typeof myVar)
